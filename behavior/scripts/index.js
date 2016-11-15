b03dbaa6805266c5ef9bd2a9a26f4cd297d91168
waglik <waglik@gmail.com>
2016-11-15 12:30:06 +0200

    Creating repository scaffolding for test.

---
+++ behavior/scripts/index.js
@@ -0,0 +1,68 @@
+'use strict'
+
+exports.handle = function handle(client) {
+  const sayHello = client.createStep({
+    satisfied() {
+      return Boolean(client.getConversationState().helloSent)
+    },
+
+    prompt() {
+      client.addResponse('app:response:name:welcome')
+      client.addResponse('app:response:name:provide/documentation', {
+        documentation_link: 'http://docs.init.ai',
+      })
+      client.addResponse('app:response:name:provide/instructions')
+      client.updateConversationState({
+        helloSent: true
+      })
+      client.done()
+    }
+  })
+
+  const untrained = client.createStep({
+    satisfied() {
+      return false
+    },
+
+    prompt() {
+      client.addResponse('app:response:name:apology/untrained')
+      client.done()
+    }
+  })
+
+  const handleGreeting = client.createStep({
+    satisfied() {
+      return false
+    },
+
+    prompt() {
+      client.addResponse('app:response:name:greeting')
+      client.done()
+    }
+  })
+
+  const handleGoodbye = client.createStep({
+    satisfied() {
+      return false
+    },
+
+    prompt() {
+      client.addResponse('app:response:name:goodbye')
+      client.done()
+    }
+  })
+
+  client.runFlow({
+    classifications: {
+      goodbye: 'goodbye',
+      greeting: 'greeting'
+    },
+    streams: {
+      goodbye: handleGoodbye,
+      greeting: handleGreeting,
+      main: 'onboarding',
+      onboarding: [sayHello],
+      end: [untrained]
+    }
+  })
+}
